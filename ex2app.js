f9c7bd12aabaf53f4a6c8ce040069fdc4375db0c
Raul Ferraz <128731015+Raulberseker@users.noreply.github.com>
2023-04-27 17:08:48 -0300

    Create ex2app.js

---
+++ ex2app.js
@@ -0,0 +1,29 @@
+import { LightningElement, track } from 'lwc';
+import styles from 'app.css';
+
+
+export default class ImcCalculator extends LightningElement {
+    @track name;
+    @track weight;
+    @track height;
+    @track imc;
+
+    handleNameChange(event) {
+        this.name = event.target.value;
+    }
+
+    handleWeightChange(event) {
+        this.weight = event.target.value;
+    }
+
+    handleHeightChange(event) {
+        this.height = event.target.value;
+    }
+
+    calculateIMC() {
+        if (this.weight && this.height) {
+            const imc = this.weight / (this.height * this.height);
+            this.imc = imc.toFixed(2);
+        }
+    }
+}
